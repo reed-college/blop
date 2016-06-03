b9efa7fbf1ac94c4c24aed5873227caaf90a35c4
Chase Doremus <chadorem@reed.edu>
2016-06-03 12:01:50 -0700

    Removing Blueprints

--- app/auth/views.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, Blueprint, render_template
-from app import app
-
-mod = Blueprint('auth', __name__)
-
-@mod.route('/login/')
-def login():
-	return render_template('auth/signin.html')
--- app/maps/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, Blueprint, render_template
-from app import app
-
-mod = Blueprint('maps', __name__)
-
-@mod.route('/')
-@mod.route('/map')
-def maps():
-	return render_template('map/map.html')
