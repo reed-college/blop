7819f8e0058c29894b7d27a3843055d4bb51b894
Chase Doremus <chadorem@reed.edu>
2016-06-16 14:01:16 -0700

    Removed general locations table from db, added general enum as column in the other locations table (simplifying schema). Updated instructions in design.md

--- scripts/manage.py
+++ scripts/manage.py
@@ -12,36 +12,31 @@ manager.add_command('db', MigrateCommand)
 
 
 @manager.command
-def seed():
-    "Add seed data to the database."
+def loc_seed():
 
-    models.SpecificLocation.query.delete()
-    models.GeneralLocation.query.delete()
-    models.Type.query.delete()
-
-    with open('general_locations.csv', 'r') as csvfile:
-        genlocreader = csv.DictReader(csvfile, delimiter=',')
-        for row in genlocreader:
-            genlocs = models.GeneralLocation(id=row['id'], name=row['name'],
-                                             oncampus=row['oncampus'])
-            db.session.add(genlocs)
+    models.Location.query.delete()
 
     with open('location_codes.csv', 'r') as csvfile:
         loccodereader = csv.DictReader(csvfile, delimiter=',')
         for row in loccodereader:
-            loccode = models.SpecificLocation(id=row['id'], name=row['name'],
-                                              general_location_id=row[
-                                              'general_location_id'])
+            loccode = models.Location(name=row['name'], general=row[
+                                              'general'])
             db.session.add(loccode)
+            db.session.commit()
+
+
+@manager.command
+def type_seed():
+
+    models.Type.query.delete()
 
     with open('incident_types.csv', 'r') as csvfile:
         typereader = csv.DictReader(csvfile, delimiter=',')
         for row in typereader:
-            typecode = models.Type(id=row['id'], code=row['code'],
+            typecode = models.Type(code=row['code'],
                                    description=row['description'])
             db.session.add(typecode)
-
-    db.session.commit()
+            db.session.commit()
 
 
 if __name__ == '__main__':
