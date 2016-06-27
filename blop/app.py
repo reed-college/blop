0aa773765e31dca0ea6f0f201ae2468676c121a6
Chase Doremus <chadorem@reed.edu>
2016-06-27 11:39:44 -0700

    adds a script to insert fake incidents into the db Incident table

--- blop/app.py
+++ blop/app.py
@@ -8,15 +8,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = db.db_url
 
 db = SQLAlchemy(app)
 
+from blop import models
+
 
 @app.route('/login/')
 def login():
     return render_template('signin.html')
 
 
-@app.route('/submit', methods=['GET','POST'])
+@app.route('/submit', methods=['GET', 'POST'])
 def submit():
-    return render_template('submit.html')
+
+    types = models.Type.query.all()
+    locations = models.Location.query.all()
+
+    return render_template('submit.html', types=types, locations=locations)
 
 
 @app.route('/blotter')
@@ -34,12 +40,37 @@ def maps():
 def search():
     return render_template('search.html')
 
+
 @app.route('/processform', methods=['GET', 'POST'])
 def processform():
-	#This is for testing.
-	#Put the database interactions for the form here.
-	incident_code=request.form['incident dropdown']
-	return "The incident type is " + str(incident_code)
+    # This is for testing.
+    # Put the database interactions for the form here.
+    f = request.form
+    codelist = []
+    for key in f.keys():
+        for value in f.getlist(key):
+            codelist.append(value)
+    return "Incident type(s):  " + str(codelist)
+    types = []
+    incident_code = request.form['incident dropdown']
+    types.append(incident_code)
+
+    location = request.form['']
+
+    datetime = request.form['']
+
+    incident_summary = request.form['incident summary']
+
+    incident = models.Incident(
+                               datetime=datetime,
+                               summary=incident_summary,
+                               location=location,
+                               types=types
+                               )
+    db.session.add(incident)
+    db.session.commit()
+
+    return "The incident type is " + str(incident_code)
 
 
 if __name__ == '__main__':
--- scripts/manage.py
+++ scripts/manage.py
@@ -1,6 +1,7 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 import csv
+import datetime
 
 from blop.app import app, db
 
@@ -13,8 +14,6 @@ manager.add_command('db', MigrateCommand)
 
 @manager.command
 def loc_seed():
-
-    models.Location.query.delete()
 
     with open('location_codes.csv', 'r') as csvfile:
         loccodereader = csv.DictReader(csvfile, delimiter=',')
@@ -42,18 +41,59 @@ def type_seed():
 @manager.command
 def add_fake_incidents():
 
+    incidents = db.session.query(models.Incident).all()
+    for incident in incidents:
+        types = incident.types
+        for t in types:
+            incident.types.remove(t)
     models.Incident.query.delete()
 
     # insert function to add fake incidents
-    # location = location = models.Location.query.filter(models.Location.name == "Foster").first()
-    # types = [models.Type.query.filter(models.Type.code == "UAA").first(), models.Type.query.filter(models.Type.code == "THEB").first(), models.Type.query.filter(models.Type.code == "HARA").first()]
-    # incident = models.Incident(
-    #                            datetime = datetime.datetime(2016, 8, 13, 19, 00)
-    #                            summary = "practice event"
-    #                            types = types
-    #                            location = location
-    # )
+    location = models.Location.query.filter(models.Location.name ==
+                                            "Foster").first()
+    types = [models.Type.query.filter(models.Type.code == "UAA").first(),
+             models.Type.query.filter(models.Type.code == "THEB").first(),
+             models.Type.query.filter(models.Type.code == "HARA").first()]
 
+    summary = "practice event in foster-unattended alc, bike theft, harassment"
+    incident = models.Incident(
+                            datetime=datetime.datetime(2016, 8, 13, 19, 00),
+                            summary=summary,
+                            types=types,
+                            location=location
+    )
+    db.session.add(incident)
+    db.session.commit()
+
+    location = location = models.Location.query.filter(models.Location.name ==
+                                                       "Chittick").first()
+    types = [models.Type.query.filter(models.Type.code == "UNLD").first(),
+             models.Type.query.filter(models.Type.code == "AODMJ").first()]
+
+    summary = "practice event in chittick-dorm unlock, marijuana AOD"
+    incident = models.Incident(
+                                datetime=datetime.datetime(2016, 4, 25, 6, 23),
+                                summary=summary,
+                                types=types,
+                                location=location
+    )
+    db.session.add(incident)
+    db.session.commit()
+
+    location = location = models.Location.query.filter(models.Location.name ==
+                                                       "Eliot Hall").first()
+    types = [models.Type.query.filter(models.Type.code == "ALF").first(),
+             models.Type.query.filter(models.Type.code == "DIS").first()]
+
+    summary = "practice event in Eliot-fire alarm, disturbance"
+    incident = models.Incident(
+                                datetime=datetime.datetime(2016, 2, 14, 18, 5),
+                                summary=summary,
+                                types=types,
+                                location=location
+    )
+    db.session.add(incident)
+    db.session.commit()
 
 
 if __name__ == '__main__':
